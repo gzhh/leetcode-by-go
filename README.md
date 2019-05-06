ee4b7e19964f332d23c45e30fe67f3bb93cf5d33
Zhonghuan Gao <zhonghuangao@gmail.com>
2019-05-06 17:27:17 +0800

    Submit 1.Two Sum

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# leetcode-by-go
-Solutions with golang
+# Leetcode solutions with golang
+
+| # | Title | Solution | Difficulty | Importantly |
+|--- | --- | ---| --- | --- |
+| 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | [Golang](algorithms/001.twoSum.go) | Easy |
---
+++ algorithms/001.twoSum.go
@@ -0,0 +1,52 @@
+// Source : https://leetcode.com/problems/two-sum/
+// Author : Zhonghuan Gao
+// Date   : 2019-05-06
+
+/********************************************************************************** 
+*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*               
+***********************************************************************************/
+
+/**
+ * 解法一：暴力法
+ * 双层循环每两个不同元素相加的值与target比较
+ */
+func twoSum(nums []int, target int) []int {
+    var res []int;
+    for i, vi := range nums {
+        for j, vj := range nums {
+            if (i != j && vi + vj == target) {
+                return append(res, i, j)
+            }
+        }
+    }
+    return res
+}
+
+/**
+ * 解法二：哈希表
+ * 利用map储存每个数组元素的值和索引，当target减掉当前数组元素的值在哈希表中存在的话，
+ * 得出两个数组元素相加等于target，最后返回两个元素在数组中的索引
+ */
+func twoSum(nums []int, target int) []int {
+    mp := make(map[int]int)
+    
+    for i, num := range(nums) {
+        if idx, ok := mp[target - num]; ok {
+            return []int{i, idx}
+        }
+        mp[num] = i
+    }
+    
+    return []int{}
+}
