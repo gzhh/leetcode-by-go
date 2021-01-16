ced5cc6e8c374d4f69e7e661bd72c1d11d66f3dd
gzhh <zhonghuangao@gmail.com>
2021-01-16 15:16:13 +0800

    Submit 95. Unique Binary Search Trees II

---
+++ algorithms/0095.UniqueBinarySearchTreesII/0095.UniqueBinarySearchTreesII.go
@@ -0,0 +1,87 @@
+// Source : https://leetcode.com/problems/unique-binary-search-trees-ii/
+// Author : Zhonghuan Gao
+// Date   : 2020-01-16
+
+/**********************************************************************************
+*
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+Example:
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+
+Constraints:
+0 <= n <= 8
+*
+***********************************************************************************/
+
+package main
+
+/**
+解法一：递归
+
+Ref: https://leetcode-cn.com/problems/unique-binary-search-trees-ii/solution/bu-tong-de-er-cha-sou-suo-shu-ii-by-leetcode-solut/
+PS: 待复盘
+*/
+/**
+* Definition for a binary tree node.
+* type TreeNode struct {
+*     Val int
+*     Left *TreeNode
+*     Right *TreeNode
+* }
+*/
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func generateTrees(n int) []*TreeNode {
+	if n == 0 {
+		return nil
+	}
+	return helper(1, n)
+}
+
+func helper(start, end int) []*TreeNode {
+	if start > end {
+		return []*TreeNode{nil}
+	}
+	allTrees := []*TreeNode{}
+	// 枚举可行根节点
+	for i := start; i <= end; i++ {
+		leftTrees := helper(start, i - 1)
+		rightTrees := helper(i + 1, end)
+		// 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+		for _, left := range leftTrees {
+			for _, right := range rightTrees {
+				currTree := &TreeNode{i, nil, nil}
+				currTree.Left = left
+				currTree.Right = right
+				allTrees = append(allTrees, currTree)
+			}
+		}
+	}
+	return allTrees
+}
+
+/**
+解法二：动态规划
+
+ */
